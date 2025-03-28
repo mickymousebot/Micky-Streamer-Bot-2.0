cb9e18c4f2639d0eff0920f5bae5c5a35cff07d4
Micky Mouse <techsashif+eufya@gmail.com>
2025-03-28 07:20:48 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
-import asyncio, logging, aiohttp, traceback
-from info import *
+import asyncio, logging, aiohttp, traceback, pytz, random, string
+from info import API, URL  
+from datetime import date 
+from shortzy import Shortzy
 
+# Verification system variables
+TOKENS = {}
+VERIFIED = {}
 #Dont Remove My Credit @Tech_Shreyansh
 #This Repo Is By @SmartEdith_Bot 
 # For Any Kind Of Error Ask Us In Support Group @Tech_Shreyansh2
@@ -71,6 +76,54 @@ def get_readable_time(seconds: int) -> str:
     time_list.reverse()
     readable_time += ": ".join(time_list)
     return readable_time 
+# Verifcation On 
+async def get_verify_shorted_link(link):
+    shortzy = Shortzy(api_key=API, base_site=URL)
+    link = await shortzy.convert(link)
+    return link
+
+async def check_token(bot, userid, token):
+    user = await bot.get_users(userid)
+    if user.id in TOKENS.keys():
+        TKN = TOKENS[user.id]
+        if token in TKN.keys():
+            is_used = TKN[token]
+            if is_used == True:
+                return False
+            else:
+                return True
+    else:
+        return False
+
+async def get_token(bot, userid, link):
+    user = await bot.get_users(userid)
+    token = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
+    TOKENS[user.id] = {token: False}
+    link = f"{link}verify-{user.id}-{token}"
+    shortened_verify_url = await get_verify_shorted_link(link)
+    return str(shortened_verify_url)
+
+async def verify_user(bot, userid, token):
+    user = await bot.get_users(userid)
+    TOKENS[user.id] = {token: True}
+    tz = pytz.timezone('Asia/Kolkata')
+    today = date.today()
+    VERIFIED[user.id] = str(today)
+
+async def check_verification(bot, userid):
+    user = await bot.get_users(userid)
+    tz = pytz.timezone('Asia/Kolkata')
+    today = date.today()
+    if user.id in VERIFIED.keys():
+        EXP = VERIFIED[user.id]
+        years, month, day = EXP.split('-')
+        comp = date(int(years), int(month), int(day))
+        if comp<today:
+            return False
+        else:
+            return True
+    else:
+        return False
 
 #Dont Remove My Credit @Tech_Shreyansh
 #This Repo Is By @SmartEdith_Bot 
